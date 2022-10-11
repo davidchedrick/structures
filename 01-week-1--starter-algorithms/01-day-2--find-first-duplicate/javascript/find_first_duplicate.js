564fd1df17a353e4be74e105d4c454e96709da8f
David Chedrick <davidchedrick@gmail.com>
2022-10-11 09:19:23 -0500

    solved js 01d1

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,16 +1,26 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+	const uniques = new Set();
+
+	for (const num of arr) {
+		if (uniques.has(num)) {
+			return num;
+		}
+		uniques.add(num);
+	}
+
+	return -1;
 }
 
+console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+	// add your own tests in here
+	console.log("Expecting: 3");
+	console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+	console.log("Expecting: -1");
+	console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
 }
 
 module.exports = findFirstDuplicate;
