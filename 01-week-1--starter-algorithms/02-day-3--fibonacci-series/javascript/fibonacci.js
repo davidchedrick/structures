4b691062aa90a2089f140c5a06ec5466f8e1bf28
David Chedrick <davidchedrick@gmail.com>
2022-10-11 09:38:31 -0500

    finished fib

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,21 +1,34 @@
 function fibonacci(num) {
-  // type your code here
+	//011235813
+
+	if (num < 2) {
+		return num;
+	}
+
+	let lastTwo = [0, 1];
+
+	for (let i = 0; i < num - 1; ++i) {
+		const sum = lastTwo[0] + lastTwo[1];
+		lastTwo = [lastTwo[1], sum];
+	}
+	return lastTwo[1];
 }
+console.log("=>", fibonacci(10));
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+	// add your own tests in here
+	console.log("Expecting: 0");
+	console.log("=>", fibonacci(0));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+	console.log("Expecting: 1");
+	console.log("=>", fibonacci(2));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+	console.log("Expecting: 55");
+	console.log("=>", fibonacci(10));
 }
 
 module.exports = fibonacci;
