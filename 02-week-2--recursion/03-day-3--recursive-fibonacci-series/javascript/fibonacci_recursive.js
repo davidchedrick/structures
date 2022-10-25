646ab927c25ace29e4b74500d12ec776ed8eb398
David Chedrick <davidchedrick@gmail.com>
2022-10-25 13:21:01 -0500

    solved recursive fab

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
@@ -1,24 +1,22 @@
 function fibonacci(n) {
-  // type your code here
+	if (n < 2) return n;
+	return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+	// add your own tests in here
+	console.log("Expecting: 0");
+	console.log("=>", fibonacci(0));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+	console.log("Expecting: 1");
+	console.log("=>", fibonacci(2));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+	console.log("Expecting: 55");
+	console.log("=>", fibonacci(10));
 }
 
 module.exports = fibonacci;
-
-// Please add your pseudocode to this file
-// And a written explanation of your solution
