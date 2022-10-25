c1996e6bf37034dc74a92839b31581d72c553706
David Chedrick <davidchedrick@gmail.com>
2022-10-25 11:53:33 -0500

    solved recursive search

--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,16 +1,24 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+	const recursiveSearch = (arr, target) => {
+		if (arr.length === 0) {
+			return false;
+		}
+		if (arr[0] === target) {
+			return true;
+		}
+		return recursiveSearch(arr.slice(1), target);
+	};
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: true");
-  console.log("=>", recursiveSearch([1, 2, 3], 2));
+	// add your own tests in here
+	console.log("Expecting: true");
+	console.log("=>", recursiveSearch([1, 2, 3], 2));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", recursiveSearch([3, 2, 1], 4));
+	console.log("Expecting: false");
+	console.log("=>", recursiveSearch([3, 2, 1], 4));
 }
 
 module.exports = recursiveSearch;
