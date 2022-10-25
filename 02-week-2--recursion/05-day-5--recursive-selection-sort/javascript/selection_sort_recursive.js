eb223587e158f977d5cda0fedd696e5be9459f6f
David Chedrick <davidchedrick@gmail.com>
2022-10-25 13:57:49 -0500

    solved selection sort recursive:

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,13 +1,24 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+	if (arr.length === 0) {
+		return [];
+	}
+
+	const min = Math.min(...arr);
+	const idx = arr.indexOf(min);
+	arr.splice(idx, 1);
+
+	const result = selectionSortRecursive(arr);
+	console.log(result);
+	result.unshift(min);
+	return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
+	// add your own tests in here
+	console.log("Expecting: [-1, 2, 3, 5]");
+	console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
-  console.log("");
+	console.log("");
 }
 
 module.exports = selectionSortRecursive;
